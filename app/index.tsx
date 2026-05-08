8d38474039a04ea997d0956459fc16be3b5e9656
Sy Rashid <sy.rashid.11@gmail.com>
2026-05-08 17:16:16 +0200

    STYLE: Add style to home screen

---
+++ app/index.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>StickerSmash Home Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "#fff",
+  },
+});
