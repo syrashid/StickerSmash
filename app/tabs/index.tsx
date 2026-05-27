e30c4e196f2bae2e6d9fff3ebca1179afa371856
Sy Rashid <sy.rashid.11@gmail.com>
2026-05-27 11:35:23 +0200

    FEAT: Add navigation for app

--- app/tabs/index.tsx
+++ app/tabs/index.tsx
@@ -5,7 +5,7 @@ export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>StickerSmash Home Screen</Text>
-      <Link href="/about" style={styles.button}>
+      <Link href="/tabs/about" style={styles.button}>
         Go to About screen
       </Link>
     </View>
